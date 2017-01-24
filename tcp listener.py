fb5eb0f5be340dd0a64b4d8d6b862f05b93313a5
Shailesh Iyer <swethashailu9696@gmail.com>
2017-01-24 16:42:16 +0530

    Add files via upload
    python tcp listener

---
+++ tcp listener.py
@@ -0,0 +1,20 @@
+import sys
+import socket
+import math
+import cv2
+
+TCP_IP = "192.168.43.140"
+TCP_PORT= 9001
+
+sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+sock.bind((TCP_IP,TCP_PORT))
+sock.listen(True)
+print 'starting while loop'
+while True:
+        conn,addr=sock.accept()
+        data = conn.recv(10000)
+        print data
+        cv2.waitKey(5) 
+sock.close()        
+conn.close()
+
