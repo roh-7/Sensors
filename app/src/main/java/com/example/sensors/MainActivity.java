7a4ff12dc6918dcb40cb0db83771627306b4e7d7
Shailesh Iyer <swethashailu9696@gmail.com>
2017-02-16 15:05:03 +0530

    video is now working via chrome custom tabs -sensors integration left

--- app/src/main/java/com/example/sensors/MainActivity.java
+++ app/src/main/java/com/example/sensors/MainActivity.java
@@ -6,36 +6,42 @@ import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Vibrator;
+import android.support.customtabs.CustomTabsIntent;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.MediaController;
 import android.widget.TextView;
+import android.widget.Toast;
 import android.widget.VideoView;
+
+//import com.github.niqdev.mjpeg.DisplayMode;
+//import com.github.niqdev.mjpeg.Mjpeg;
+//import com.github.niqdev.mjpeg.MjpegView;
 
 import java.io.BufferedWriter;
 import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
 import java.net.InetAddress;
 import java.net.Socket;
-import java.security.SecureRandom;
-import java.security.cert.CertificateException;
-import java.security.cert.X509Certificate;
 
-import javax.net.ssl.HostnameVerifier;
-import javax.net.ssl.HttpsURLConnection;
-import javax.net.ssl.SSLContext;
-import javax.net.ssl.SSLSession;
-import javax.net.ssl.X509TrustManager;
 
 public class MainActivity extends AppCompatActivity implements SensorEventListener {
 
-    PostToDB postToDB;
+    //PostToDB postToDB;
     //public int c = 0;
+    //@BindView(R.id.VIEW_NAME)
+//    MjpegView mjpegView;
 
     public VideoView video;
     public EditText address;
@@ -62,12 +68,14 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     public float deltaY = 0;
     public float deltaZ = 0;
 
-    Thread newThread = new Thread(new ClientThre());
+    //Thread newThread = new Thread(new ClientThre());
 
 
     public float delGX = 0;
     public float delGY = 0;
     public float delGZ = 0;
+
+    int TIMEOUT = 5; //seconds
 
     public Button btn;
 
@@ -79,17 +87,18 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
 
     public Vibrator v;
 
+    public WebView webView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        final MediaController mediaController = new MediaController(this);
+//        MediaController mediaController = new MediaController(this);
         setContentView(R.layout.activity_main);
-        initialiseViews();
-        video = (VideoView) findViewById(R.id.Video);
+//        video = (VideoView) findViewById(R.id.Video);
         ipade = (EditText) findViewById(R.id.ipad);
         porte = (EditText) findViewById(R.id.port);
-        Log.v("tag", "tag");
+        Log.v("tag","tag");
         btn = (Button) findViewById(R.id.Start);
+//        webView = (WebView) findViewById(R.id.webview1);
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -97,20 +106,24 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
                     address = (EditText) findViewById(R.id.StreamAddress);
                     String adds = address.getText().toString();
                     String address = "http://" + adds;
-                    Log.v("tag", address);
-                    Uri uri = Uri.parse(address);
-                    Log.v("tag", "tag12");
-
-                    video.setMediaController(mediaController);
+                    Log.v("tag",address);
+//                    Uri uri = Uri.parse(address);
+                    Log.v("tag","tag12");
+                    /*video.setMediaController(mediaController);
                     video.setVideoURI(uri);
-                    Log.v("tag", "tag433");
-                    trustEveryone();
-                    video.start();
-                } catch (Exception e) {
+                    Log.v("tag","tag433");
+
+                    video.start();*/
+                    //loadipcam new method for creating a webview doesnt work
+                    loadipcam(address);
+
+                }
+                catch(Exception e){
                     e.printStackTrace();
                 }
             }
         });
+
 
 
         sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
@@ -128,12 +141,38 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
             gyroscope = getSensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE);
             getSensorManager.registerListener(this, gyroscope, SensorManager.SENSOR_DELAY_NORMAL);
         }
-//        btn = (Button) findViewById(R.id.btn);
 //        btn.setOnClickListener(connectListener);
+
 
 
         v = (Vibrator) this.getSystemService(Context.VIBRATOR_SERVICE);
     }
+
+    private void loadipcam(String adds){
+        /*Tried this but this aint working as it sends a single request for the page*/
+        Uri uri =Uri.parse(adds);
+        CustomTabsIntent.Builder intentBuilder = new CustomTabsIntent.Builder();
+
+// Begin customizing
+// set toolbar colors
+        intentBuilder.setToolbarColor(ContextCompat.getColor(this, R.color.colorPrimary));
+        intentBuilder.setSecondaryToolbarColor(ContextCompat.getColor(this, R.color.colorPrimaryDark));
+
+// set start and exit animations
+//        intentBuilder.setStartAnimations(this, R.anim.slide_in_right, R.anim.slide_out_left);
+//        intentBuilder.setExitAnimations(this, android.R.anim.slide_in_left,
+//                android.R.anim.slide_out_right);
+
+// build custom tabs intent
+        CustomTabsIntent customTabsIntent = intentBuilder.build();
+
+// launch the url
+        customTabsIntent.launchUrl(this, uri);
+
+    }
+
+
+
 
     public class ClientThre implements Runnable {
         PrintWriter out;
@@ -142,15 +181,12 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         public void run() {
             try {
                 InetAddress serverAddr = InetAddress.getByName(ip);
-                Log.v("boo", "hii");
                 socket = new Socket(serverAddr, port);
-                Log.v("boo", "kjkh");
                 out = new PrintWriter(new BufferedWriter(new OutputStreamWriter(socket.getOutputStream())), true);
-                Log.v("boo", "gfds");
+                Log.v("INSIDE", "sending sensor data to remote host");
                 while (true) {
                     out.printf("A:%10.2f\t %10.2f\t %10.2f\n", deltaX, deltaY, deltaZ);
                     out.printf("G:%10.2f\t %10.2f\t %10.2f\n", delGX, delGY, delGZ);
-                    Log.v("boo", "ijo");
                     out.flush();
                     Thread.sleep(2);
                     if (!connected) {
@@ -158,22 +194,20 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
                     }
                 }
             } catch (Exception e) {
-                Log.v("boo", "uidfn");
+
                 e.printStackTrace();
             } finally {
                 try {
                     Log.v("socket", "closed");
                     socket.close();
                 } catch (Exception e) {
-                    Log.v("boo", "fiajcak");
+                    Log.v("end of sensor thread", "reached");
                     e.printStackTrace();
                 }
             }
 
         }
     }
-
-    ;
     private Button.OnClickListener connectListener = new Button.OnClickListener() {
         @Override
         public void onClick(View v) {
@@ -195,19 +229,6 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         }
     };
 
-    public void initialiseViews() {
-//        currentX = (TextView) findViewById(R.id.currentX);
-//        currentY = (TextView) findViewById(R.id.currentY);
-//        currentZ = (TextView) findViewById(R.id.currentZ);
-//
-//        maxX = (TextView) findViewById(R.id.maxX);
-//        maxY = (TextView) findViewById(R.id.maxY);
-//        maxZ = (TextView) findViewById(R.id.maxZ);
-//
-//        gyroX = (TextView) findViewById(R.id.GyroX);
-//        gyroY = (TextView) findViewById(R.id.GyroY);
-//        gyroZ = (TextView) findViewById(R.id.GyroZ);
-    }
 
     //register the sensor onResume
     @Override
@@ -261,128 +282,7 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         //v.vibrate(600);
     }
 
-//    public void displayCleanValues() {
-//        currentX.setText("0.0");
-//        currentY.setText("0.0");
-//        currentZ.setText("0.0");
-//
-//
-//        gyroX.setText("0.0");
-//        gyroY.setText("0.0");
-//        gyroZ.setText("0.0");
-//    }
-
-//    public void displayCurrentValues() {
-//        //c++;
-//        String curx = Float.toString(deltaX);
-//        String cury = Float.toString(deltaY);
-//        String curz = Float.toString(deltaZ);
-//        currentX.setText(curx);
-//        currentY.setText(cury);
-//        currentZ.setText(curz);
-//
-//        String curgx = Float.toString(delGX);
-//        String curgy = Float.toString(delGY);
-//        String curgz = Float.toString(delGZ);
-//
-//        gyroX.setText(curgx);
-//        gyroY.setText(curgy);
-//        gyroZ.setText(curgz);
-
-    //String time = new SimpleDateFormat("DD/MM/YYYY.HH.mm.ss").format(new Timestamp(System.currentTimeMillis()));
-    //Log.v("time", time);
-    //postToDB = new PostToDB(curx, cury, curz, time);
-    //if(c%678==0)
-    //postToDB.post();
-
-    private void trustEveryone() {
-        try {
-            HttpsURLConnection.setDefaultHostnameVerifier(new HostnameVerifier() {
-                public boolean verify(String hostname, SSLSession session) {
-                    return true;
-                }
-            });
-            SSLContext context = SSLContext.getInstance("TLS");
-            context.init(null, new X509TrustManager[]{new X509TrustManager() {
-                public void checkClientTrusted(X509Certificate[] chain,
-                                               String authType) throws CertificateException {
-                }
-
-                public void checkServerTrusted(X509Certificate[] chain,
-                                               String authType) throws CertificateException {
-                }
-
-                public X509Certificate[] getAcceptedIssuers() {
-                    return new X509Certificate[0];
-                }
-            }}, new SecureRandom());
-            HttpsURLConnection.setDefaultSSLSocketFactory(
-                    context.getSocketFactory());
-        } catch (Exception e) { // should never happen
-            e.printStackTrace();
-        }
-    }
-
-   /* private static OkHttpClient getUnsafeOkHttpClient() {
-        try {
-            // Create a trust manager that does not validate certificate chains
-            final TrustManager[] trustAllCerts = new TrustManager[] {
-                    new X509TrustManager() {
-                        @Override
-                        public void checkClientTrusted(java.security.cert.X509Certificate[] chain, String authType) throws CertificateException {
-                        }
-
-                        @Override
-                        public void checkServerTrusted(java.security.cert.X509Certificate[] chain, String authType) throws CertificateException {
-                        }
-
-                        @Override
-                        public java.security.cert.X509Certificate[] getAcceptedIssuers() {
-                            return new java.security.cert.X509Certificate[]{};
-                        }
-                    }
-            };
-
-            // Install the all-trusting trust manager
-            final SSLContext sslContext = SSLContext.getInstance("SSL");
-            sslContext.init(null, trustAllCerts, new java.security.SecureRandom());
-            // Create an ssl socket factory with our all-trusting manager
-            final SSLSocketFactory sslSocketFactory = sslContext.getSocketFactory();
-
-            OkHttpClient.Builder builder = new OkHttpClient.Builder();
-            builder.sslSocketFactory(sslSocketFactory);
-            builder.hostnameVerifier(new HostnameVerifier() {
-                @Override
-                public boolean verify(String hostname, SSLSession session) {
-                    return true;
-                }
-            });
-
-            OkHttpClient okHttpClient = builder.build();
-            return okHttpClient;
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
-    }*/
-
 }
-
-//    public void displayMaxValues() {
-//        if (deltaX > deltaXMax) {
-//            deltaXMax = deltaX;
-//            maxX.setText(Float.toString(deltaXMax));
-//        }
-//        if (deltaY > deltaYMax) {
-//            deltaYMax = deltaY;
-//            maxY.setText(Float.toString(deltaYMax));
-//        }
-//        if (deltaZ > deltaZMax) {
-//            deltaZMax = deltaZ;
-//            maxZ.setText(Float.toString(deltaZMax));
-//        }
-//
-//    }
-
 
 
 
--- app/src/main/java/com/example/sensors/webActivity.java
+++ app/src/main/java/com/example/sensors/webActivity.java
@@ -3,11 +3,11 @@ package com.example.sensors;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class feed extends AppCompatActivity {
-    /*SHAILESH : Ithought of creating a new activity just for showing the stream*/
+public class webActivity extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_feed);
+        setContentView(R.layout.activity_web);
     }
 }
